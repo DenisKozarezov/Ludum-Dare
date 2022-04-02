1caf3ff0e22fdad4e8b778a2e845057047ae8666
samaxe13 <danek.il420@gmail.com>
2022-04-02 22:29:59 +0300

    adden UnityEvents

--- Assets/Scripts/Views/EnergyDistribution.cs
+++ Assets/Scripts/Views/EnergyDistribution.cs
@@ -1,20 +1,27 @@
 using UnityEngine;
 using UnityEngine.UI;
-using TMPro;
+using UnityEngine.Events;
 
 public class EnergyDistribution : MonoBehaviour
 {
     public Energy energy = new Energy();
+    public Slider percentageSlider;
+    public Slider spentSlider;
     public float timer = 1f;
     public float maxTimer = 1f;
-    public Slider percentageSlider;
     public float targetEnergyCount = 1000f;
 
-    private void Start() {
-        percentageSlider.value = 50f;        
+    public UnityEvent OnValueChange;
+    public UnityEvent Win;
+
+    private void Start() 
+    {
+        percentageSlider.value = 50f;
+        OnValueChange?.Invoke();
     }
 
-    private void Update() {
+    private void Update() 
+    {
         if (timer > 0) timer -= Time.deltaTime;
         else
         {
@@ -22,25 +29,27 @@ public class EnergyDistribution : MonoBehaviour
             {
                 energy.count -= energy.spentPerSecond;
             }
+            Debug.Log(energy.count);
             energy.count += energy.incomePerSecond + energy.incomePerSecond*energy.efficiency*(energy.energyPercentage/100f);
             timer = maxTimer;
+            OnValueChange?.Invoke();
+            spentSlider.maxValue = energy.count;
             if (energy.count >= targetEnergyCount) 
             {
-                Debug.Log("Win!");
+                Win?.Invoke();
             }
         }
     }
 
-    public void Spent(float increment)
+    public void Spent()
     {
-        if (energy.spentPerSecond > 0 || increment > 0) {
-            energy.spentPerSecond += increment;
-        }
+        energy.spentPerSecond = spentSlider.value;
     }
 
     public void DistributeEnergy() 
     {
         energy.armyPercentage = percentageSlider.value;
         energy.energyPercentage = 100f - percentageSlider.value;
+        OnValueChange?.Invoke();
     }
 }
--- Assets/Scripts/Views/EnergyUI.cs
+++ Assets/Scripts/Views/EnergyUI.cs
@@ -1,35 +1,26 @@
 using UnityEngine;
-using UnityEngine.UI;
+using UnityEngine.Events;
 using System;
 using TMPro;
 
 public class EnergyUI : MonoBehaviour
 {
     public EnergyDistribution energyDistribution;
-    private float income = 0;
     public TextMeshProUGUI energyTextMesh;
     public TextMeshProUGUI percentage;
-
+    
+    private float income = 0;
 
     void Start()
     {
+        if (energyDistribution.OnValueChange == null) energyDistribution.OnValueChange = new UnityEvent();
+        energyDistribution.OnValueChange.AddListener(UpdateValues);
+    }
+
+    public void UpdateValues()
+    {
         income = energyDistribution.energy.incomePerSecond + energyDistribution.energy.incomePerSecond * energyDistribution.energy.efficiency * (energyDistribution.energy.energyPercentage / 100f);
         energyTextMesh.text = String.Format(EnergyText.EnergyCount, energyDistribution.energy.count, energyDistribution.maxTimer, income, energyDistribution.energy.incomePerSecond, energyDistribution.maxTimer, energyDistribution.energy.spentPerSecond);
-        percentage.text = String.Format(EnergyText.PercentageCount, energyDistribution.energy.armyPercentage, energyDistribution.energy.energyPercentage);
-    }
-
-    void Update()
-    {
-        if(energyDistribution.timer <= 0) 
-        {
-            energyTextMesh.text = String.Format(EnergyText.EnergyCount, energyDistribution.energy.count, energyDistribution.maxTimer, income, energyDistribution.energy.incomePerSecond, energyDistribution.maxTimer, energyDistribution.energy.spentPerSecond);
-            income = energyDistribution.energy.incomePerSecond + energyDistribution.energy.incomePerSecond * energyDistribution.energy.efficiency * (energyDistribution.energy.energyPercentage / 100f);
-            percentage.text = String.Format(EnergyText.PercentageCount, energyDistribution.energy.armyPercentage, energyDistribution.energy.energyPercentage);
-        }
-    }
-
-    public void ChangeValues()
-    {
         percentage.text = String.Format(EnergyText.PercentageCount, energyDistribution.energy.armyPercentage, energyDistribution.energy.energyPercentage);
     }
 }
