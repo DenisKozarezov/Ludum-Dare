3b18cbb3263d0ecfba6d4c08fb450fcda1a7182a
samaxe13 <danek.il420@gmail.com>
2022-04-02 16:58:42 +0300

    fixed energy count

--- Assets/Scripts/Views/EnergyDistribution.cs
+++ Assets/Scripts/Views/EnergyDistribution.cs
@@ -1,34 +1,53 @@
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class EnergyDistribution : MonoBehaviour
 {
     public Energy energy = new Energy();
-    public float newPercentage;
-    public float newSpentPerSecond;
     public float timer = 1f;
+    public float maxTimer = 1f;
     public TextMeshProUGUI energyText;
-    public TextMeshProUGUI procentage;
+    public TextMeshProUGUI percentage;
+    public Slider percentageSlider;
+    public float targetEnergyCount = 1000f;
 
     private void Start() {
-        DistributeEnergy();
-        energyText.text = $"Energy: {energy.count}\nIncome({timer}s): {energy.incomePerSecond}\nSpent({timer}s): {energy.spentPerSecond}";
+        percentageSlider.value = 50f;
+        energyText.text = $"Energy: {energy.count}\nIncome({maxTimer}s): {energy.incomePerSecond + energy.incomePerSecond*energy.efficiency*(energy.energyPercentage/100f)}({energy.incomePerSecond})\nSpent({maxTimer}s): {energy.spentPerSecond}";
+        percentage.text = $"Army: {energy.armyPercentage}%\nEnergy: {energy.energyPercentage}%";
     }
 
     private void Update() {
         if (timer > 0) timer -= Time.deltaTime;
         else
         {
-            energy.count += energy.incomePerSecond - energy.spentPerSecond;
-            timer = 1f;
-            energyText.text = $"Energy: {energy.count}\nIncome({timer}s): {energy.incomePerSecond}\nSpent({timer}s): {energy.spentPerSecond}";
+            if (energy.count >= energy.spentPerSecond) 
+            {
+                energy.count -= energy.spentPerSecond;
+            }
+            energy.count += energy.incomePerSecond + energy.incomePerSecond*energy.efficiency*(energy.energyPercentage/100f);
+            timer = maxTimer;
+            energyText.text = $"Energy: {energy.count}\nIncome({maxTimer}s): {energy.incomePerSecond + energy.incomePerSecond*energy.efficiency*(energy.energyPercentage/100f)}({energy.incomePerSecond})\nSpent({maxTimer}s): {energy.spentPerSecond}";
+            if (energy.count >= targetEnergyCount) 
+            {
+                Debug.Log("Win!");
+            }
         }
     }
 
-    void DistributeEnergy() 
+    public void Spent(float increment)
     {
-        energy.armyPercentage = newPercentage;
-        energy.energyPercentage = 100f - newPercentage;
-        procentage.text = $"Army: {energy.armyPercentage}%\nEnergy: {energy.energyPercentage}%";
+        if (energy.spentPerSecond > 0 || increment > 0) {
+            energy.spentPerSecond += increment;
+            energyText.text = $"Energy: {energy.count}\nIncome({maxTimer}s): {energy.incomePerSecond + energy.incomePerSecond*energy.efficiency*(energy.energyPercentage/100f)}({energy.incomePerSecond})\nSpent({maxTimer}s): {energy.spentPerSecond}";
+        }
+    }
+
+    public void DistributeEnergy() 
+    {
+        energy.armyPercentage = percentageSlider.value;
+        energy.energyPercentage = 100f - percentageSlider.value;
+        percentage.text = $"Army: {energy.armyPercentage}%\nEnergy: {energy.energyPercentage}%";
     }
 }
