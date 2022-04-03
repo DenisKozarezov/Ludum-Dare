f8acf7e8950092672d68121b1df7d6e610073c4f
samaxe13 <danek.il420@gmail.com>
2022-04-03 13:53:51 +0300

    fixed energy count

--- Assets/Scripts/Model/Data/EnergyText.cs
+++ Assets/Scripts/Model/Data/EnergyText.cs
@@ -1,6 +1,7 @@
 
 public static class EnergyText
 {
-    public static string EnergyCount = "Energy: {0}\nIncome({1}s): {2}({3})\nSpent({4}s): {5}";
+    public static string EnergyCount = "Energy: {0:0}\nIncome({1}s): {2:0}({3:0})\nSpent({4}s): {5:0}";
     public static string PercentageCount = "Army: {0}%\nEnergy: {1}%";
+    public static string SpentPercent = "Spent: {0}%";
 }
--- Assets/Scripts/Views/EnergyUI.cs
+++ Assets/Scripts/Views/EnergyUI.cs
@@ -7,6 +7,7 @@ public class EnergyUI : MonoBehaviour
 {
     public EnergyDistribution energyDistribution;
     public TextMeshProUGUI energyTextMesh;
+    public TextMeshProUGUI spentTextMesh;
     public TextMeshProUGUI percentage;
     
     private float income = 0;
@@ -20,8 +21,9 @@ public class EnergyUI : MonoBehaviour
 
     public void UpdateValues()
     {
-        income = energyDistribution.energy.incomePerSecond + energyDistribution.energy.incomePerSecond * energyDistribution.energy.efficiency * (energyDistribution.energy.energyPercentage / 100f);
+        income = energyDistribution.energy.incomePerSecond + energyDistribution.energy.efficiency * (energyDistribution.energy.spentPerSecond*(energyDistribution.energy.energyPercentage / 100f));
         energyTextMesh.text = String.Format(EnergyText.EnergyCount, energyDistribution.energy.count, energyDistribution.maxTimer, income, energyDistribution.energy.incomePerSecond, energyDistribution.maxTimer, energyDistribution.energy.spentPerSecond);
         percentage.text = String.Format(EnergyText.PercentageCount, energyDistribution.energy.armyPercentage, energyDistribution.energy.energyPercentage);
+        spentTextMesh.text = String.Format(EnergyText.SpentPercent, energyDistribution.spentSlider.value);
     }
 }
